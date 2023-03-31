ecc6b66ee9a1a4b05839176900fdadee867458ea
mojiangpng <129439469+mojiangpng@users.noreply.github.com>
2023-03-31 08:13:33 +0800

    Add files via upload

---
+++ src/default.ts
@@ -0,0 +1,12 @@
+export const defaultSetting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  systemRule: ""
+}
+
+export const defaultMessage = `
+- 本站免费使用公共的KEY，如有需要可填入自己的 key 。
+- 本站功能强大，响应速度快，欢迎自部署。
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。
