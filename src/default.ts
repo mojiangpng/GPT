9209320090c776784fac9e8e4b09d940c722e292
mojiangpeng <20011800@qq.com>
2023-04-02 23:32:36 +0800

    修改

--- src/default.ts
+++ src/default.ts
@@ -7,6 +7,6 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用。
-- 本站功能强大，响应速度快，欢迎自部署。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 本站仅用于演示，填入自己的 key 才可使用。需要 key 请扫上方二维码联系。
+- 本站功能强大，响应速度快，自部署仅需99元，可永久单独使用。
+- Shift键 + Enter键 换行。开头输入 / 或者 空格 搜索 Prompt 预设。点击输入框滚动到底部。`
